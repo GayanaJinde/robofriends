d9733da80544cd6d956f593a67914a7133629b3a
GayanaJinde <jindegayana@gmail.com>
2022-07-02 20:23:26 +0200

    Updates

---
+++ precache-manifest.1901f9a12f8bdfe3d570ed15de5fea78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c8f7401530ebc18302b",
+    "url": "/robofriends/static/css/main.c5f323d7.chunk.css"
+  },
+  {
+    "revision": "3c8f7401530ebc18302b",
+    "url": "/robofriends/static/js/main.e8d254f6.chunk.js"
+  },
+  {
+    "revision": "75dcfc39fa8879b648fd",
+    "url": "/robofriends/static/js/runtime~main.e6791cfe.js"
+  },
+  {
+    "revision": "72838d18bd8602ec8d9e",
+    "url": "/robofriends/static/css/2.c6251af5.chunk.css"
+  },
+  {
+    "revision": "72838d18bd8602ec8d9e",
+    "url": "/robofriends/static/js/2.74959997.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b163b651474907a05f4e5ab257a1e0e8",
+    "url": "/robofriends/index.html"
+  }
+];
