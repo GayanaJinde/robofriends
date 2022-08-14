4f2be4ff7253e759e96a4edc4fb8ce9df49eb53d
GayanaJinde <jindegayana@gmail.com>
2022-08-14 22:19:30 +0200

    modified to react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,46 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Scroll from '../components/Scroll';
 import CardList from '../components//CardList';
 import SearchBox from '../components//SearchBox.js';
 import '../containers/App.css';
+//useEffect lets us hook to a feature of react where we deal with lifecycle methods
+//useState does the same for state variables
 
-class App extends Component{
-    constructor(){
-        super();
-        this.state={
-            robots: [],
-            searchField : '',
-        }
+function App() {
+
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState(''); // set to initial state
+
+    const onInput = (event)=>{
+        setSearchField(event.target.value);
     }
-
-    onInput = (event)=>{
-        this.setState({searchField : event.target.value});
-    }
-
-    render(){
-        const { robots, searchField } = this.state;
-        const filteredCards = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
-        })
-        return !robots.length ?
+    const filteredCards = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLocaleLowerCase());
+    })
+    return !robots.length ?
         <h1 className='tc'>LOADING!!!</h1> :
         (
             <div className='tc'>
                 <h1 className='f1'>ROBOGROUP</h1>
-                <SearchBox searchChange={this.onInput}/>
+                <button onClick={setCount}>Click Me!</button>
+                <SearchBox searchChange={onInput}/>
                 <Scroll>
                     <CardList robots={filteredCards}/>
                 </Scroll>
             </div>
         );
-        
-    }
 
-    componentDidMount(){
+    useEffect(()=>{ //runs everytime function App runs
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users=> this.setState({robots: users}));
-    }
+        .then(users=> {setRobots(users)});
+    }, []) //renders only once, initially bcoz we have not given any arguments
 }
 
 export default App;
